51f8818072f03868e2fbb15052e285fddb49077a
OnlineNomad <singularity123321@gmail.com>
2023-02-23 16:02:56 +0400

    Add comparing function

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let varOne;
+let varTwo;
+
+function equalityCompare(a, b) {
+    if(a == b) {
+        console.log("a and b are equal");
+    }else {
+        console.log("a and b are NOT equal");
+    }
+}
+
+varOne = 'hello';
+varTwo = 'hello';
+equalityCompare(a, b);
+varOne = 'hell';
+varTwo = 20;
+equalityCompare(a, b);
