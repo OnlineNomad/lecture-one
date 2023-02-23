4851d78b8e285fb39de086f09640d2857c0011c0
OnlineNomad <singularity123321@gmail.com>
2023-02-23 16:18:24 +0400

    Add temperature conversion function

--- js/script.js
+++ js/script.js
@@ -3,15 +3,29 @@ let varTwo;
 
 function equalityCompare(a, b) {
     if(a == b) {
-        console.log("a and b are equal");
+        console.log(a + " and " + b + " are equal");
     }else {
-        console.log("a and b are NOT equal");
+        console.log(a + " and " + b + " are NOT equal");
+    }
+}
+
+function degreeConversion(a){
+    if (typeof a == "number") {
+        let tmpr = ((a-32)*5)/9;
+        return tmpr;
+    }else {
+        return false;
     }
 }
 
 varOne = 'hello';
 varTwo = 'hello';
-equalityCompare(a, b);
+equalityCompare(varOne, varTwo);
 varOne = 'hell';
 varTwo = 20;
-equalityCompare(a, b);
+equalityCompare(varOne, varTwo);
+
+varOne = 32;
+console.log(varOne +" Fahrenheit in Celsius is: " + degreeConversion(varOne));
+varOne = '32';
+console.log(degreeConversion(varOne));
